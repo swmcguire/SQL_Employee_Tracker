25f66c4f2fe2567bfa18751d6c8081cb408c275c
swmcguire <swmcguire1@comcast.net>
2023-01-26 14:07:42 -0600

    wrote all the insert/ update code/ select code - working on adding the variable to the insert

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const cTable = require('console.table');
 const inquirer = require('inquirer');
-const mysql = require ('mysql2');
+const mysql = require('mysql2');
 
 const db = mysql.createConnection(
     {
@@ -15,24 +15,55 @@ const db = mysql.createConnection(
 
 //---------- CREATE COMMAND PROMPTS ----------//
 const action = [
-{
-    type: "list",
-    name: "action",
-    message: "Please choose action below",
-    choices: [
-        'View all departments', 
-        'View all roles',
-        'View all employees',
-        'Add a department', 
-        'Add a role',
-        'Add an employee',
-        'Update an employee role'
-    ],
-},
+    {
+        type: "list",
+        name: "action",
+        message: "Please choose action below",
+        choices: [
+            'View all departments',
+            'View all roles',
+            'View all employees',
+            'Add a department',
+            'Add a role',
+            'Add an employee',
+            'Update an employee role'
+        ]
+    },
+    {
+        type: "input",
+        name: "newDept",
+        message: "What is the new department?",
+        when: (answers) => {
+            if (answers.action === 'Add a department') {
+                return true;
+            };
+        },
+    },
+  /*  {
+        type: "input",
+        name: "newTitle",
+        message: "What is the new department?",
+        when: (answers) => {
+            if (answers.action === 'Add a role') {
+                return true;
+            };
+        },
+    },
+    {
+        type: "input",
+        name: "newDept",
+        message: "What is the new department?",
+        when: (answers) => {
+            if (answers.action === 'Add a role') {
+                return true;
+            };
+        },
+    },*/
 ];
 
+
 inquirer.prompt(action).then((answers) => {
-    console.log(answers.action);
+    console.log(answers.newDept);
     switch (answers.action) {
         case 'View all departments':
             viewDept();
@@ -46,18 +77,18 @@ inquirer.prompt(action).then((answers) => {
         case 'Add a department':
             addDept();
             break;
-        case'Add a role':
+        case 'Add a role':
             addRole();
             break;
-        case'Add an employee':
+        case 'Add an employee':
             addEmp();
             break;
-        case'Update an employee role':
+        case 'Update an employee role':
             updateEmp();
             break;
-        default:    
+        default:
             return `Please make a selection`;
-    } 
+    }
 }
 );
 
@@ -66,43 +97,53 @@ inquirer.prompt(action).then((answers) => {
 //--------VIEW ALL DEPTS
 function viewDept() {
     db.query('select * from department;', function (err, results) {
-    console.table(results);
-});
+        console.table(results);
+    });
 };
 
 //--------VIEW ALL ROLES
 function viewRoles() {
     db.query('SELECT r.id, r.title, d.name, r.salary FROM role r INNER JOIN department d on r.department_id = d.id;', function (err, results) {
         console.table(results);
-});
+    });
 };
-
 
 //--------VIEW ALL EMPLOYEES
-function viewEmps(){
+function viewEmps() {
     db.query('select e.id, e.first_name, e.last_name, r.title, d.name as department, r.salary, CONCAT(e2.first_name, " ", e2.last_name) as manager FROM department d INNER JOIN role r on d.id = r.department_id INNER JOIN employee e on e.role_id = r.id LEFT JOIN employee e2 on e2.id = e.manager_id ;', function (err, results) {
         console.table(results);
-});
+    });
 };
-
 
 
 //---------- CREATE QUERIES TO UPDATE TABLES ----------//
 
 //--------ADD DEPARTMENT 
-    //-----INSERT INTO department
-    //     VALUES ('dept_name')
-
+function addDept(answers) {
+    db.query(`INSERT INTO department(name) VALUES(newDept);`, function (err, results) {
+        console.table(`${newDept} Added`);
+    });
+};
 
 //--------ADD ROLE
-    //-----INSERT INTO ROLE
-    //     VALUES ('title','salary', 'dept_id')
-
-
+function addRole() {
+    db.query(`INSERT INTO role(title,salary,department_id) VALUES('a',1,2);`, function (err, results) {
+        console.table(`Roles Added`);
+    });
+};
 
 //--------ADD EMPLOYEE 
-    //-----INSERT INTO employee
-    //     VALUES('first_name','last_name','role','manager')
+
+function addEmp() {
+    db.query(`INSERT INTO employee(first_name, last_name, role_id,manager_id) VALUES('a','b',2,1);`, function (err, results) {
+        console.table(`Employee Added`);
+    });
+};
 
 //--------UPDATE EMPLOYEE 
-    //-----UPDATE employee SET first_name = 'first_name', last_name='last_name, role='role',manager='manager' WHERE id = ''
+
+function updateEmp() {
+    db.query(`UPDATE employee SET ____ =_____ WHERE id = ___);`, function (err, results) {
+        console.table(`Employee Updated`);
+    });
+};
