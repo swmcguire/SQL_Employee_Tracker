7e12dd9fa6ea40ec659fae130f4dda51484828ef
swmcguire <swmcguire1@comcast.net>
2023-01-26 10:35:59 -0600

    wrote scripts to update and query tables - need to write javascript next

--- db/schema.sql
+++ db/schema.sql
@@ -24,7 +24,7 @@ CREATE TABLE employee(
     first_name VARCHAR(30) NOT NULL, 
     last_name VARCHAR(30) NOT NULL, 
     role_id INT NOT NULL, 
-    manager_id INT NOT NULL,
+    manager_id INT NULL,
     FOREIGN KEY (role_id)
     REFERENCES role(id)
     ON DELETE CASCADE,
--- server.js
+++ server.js
@@ -20,29 +20,48 @@ const db = mysql.createConnection(
 
 //---------- CREATE COMMAND PROMPTS ----------//
 
+'View all departments', 
+'View all roles',
+'View all employees',
+'Add a department', 
+'Add a role',
+'Add an employee',
+'Update an employee role'
 
 
 
 //---------- CREATE QUERIES TO RETURN DATA ----------//
 
 //--------VIEW ALL DEPTS
+//----SELECT * FROM department
 
 //--------VIEW ALL ROLES
+//----SELECT r.id, r.title, d.name, r.salary FROM role r
+//    INNER JOIN department d on r.department_id = d.id
 
 //--------VIEW ALL EMPLOYEES
-//-----JOIN ALL TABLES TOGETHER
+//-----SELECT e.id, e.first_name, e.last_name, r.title, d.name, r.salary,e.manager_id
+//       FROM employee e
+//     INNER JOIN role r on e.role_id = r.id
+//     INNER JOIN department d on d.id = r.department_id
 
 
 //---------- CREATE QUERIES TO UPDATE TABLES ----------//
 
 //--------ADD DEPARTMENT 
-    //-----UPDATE TABLE DEPARTMENT
+    //-----INSERT INTO department
+    //     VALUES ('dept_name')
 
 
 //--------ADD ROLE
-    //-----UPDATE TABLE ROLES
+    //-----INSERT INTO ROLE
+    //     VALUES ('title','salary', 'dept_id')
 
 
 
 //--------ADD EMPLOYEE 
-    //-----UPDATE TABLE EMPLOYEE
+    //-----INSERT INTO employee
+    //     VALUES('first_name','last_name','role','manager')
+
+//--------UPDATE EMPLOYEE 
+    //-----UPDATE employee SET first_name = 'first_name', last_name='last_name, role='role',manager='manager' WHERE id = ''
